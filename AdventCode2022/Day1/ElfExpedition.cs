0c9cb8c19acda673a11a2958c4904370c4b2967a
witkos1989 <witkos1989@live.com>
2023-08-20 20:10:46 +0200

    Solutions for day one

---
+++ AdventCode2022/Day1/ElfExpedition.cs
@@ -0,0 +1,81 @@
+﻿namespace AdventCode2022.Day1;
+
+public sealed class ElfExpedition
+{
+	private readonly List<List<int>> _data = new();
+
+	public ElfExpedition()
+	{
+		string currentDirectory = Path.GetDirectoryName(
+		Path.GetDirectoryName(
+			Path.GetDirectoryName(
+				Directory.GetCurrentDirectory())))!;
+		string archiveFolder = Path.Combine(currentDirectory, "Day1");
+		StreamReader file = new(archiveFolder + "/FoodCaloriesDistribution.txt");
+
+		_data = ImportData(file).ToList();
+	}
+
+	public int[] Solutions()
+	{
+		int[] results = new int[2];
+
+		results[0] = ElfWithMaximalCalories();
+
+		results[1] = TopThreeElvesWithMaximalCalories();
+
+		return results;
+	}
+
+	private int ElfWithMaximalCalories()
+	{
+		IEnumerable<int> summedCalories = SumCalories(_data);
+
+		return summedCalories.Max();
+	}
+
+	private int TopThreeElvesWithMaximalCalories()
+	{
+		IEnumerable<int> summedCalories = SumCalories(_data).
+		OrderDescending();
+
+		return summedCalories.Take(3).Sum();
+	}
+
+	private static IEnumerable<int> SumCalories(List<List<int>> elfFoods)
+	{
+		foreach (List<int> foods in elfFoods)
+		{
+			int caloriesSum = foods.Sum();
+
+			yield return caloriesSum;
+		}	
+	}
+
+	private static IEnumerable<List<int>> ImportData(StreamReader file)
+	{
+		bool endOfFile = false;
+
+		while (!endOfFile)
+		{
+			List<int> elfInventory = new();
+
+			for (; ; )
+			{
+				string? caloriesString = file.ReadLine();
+
+				if (string.IsNullOrEmpty(caloriesString))
+					break;
+
+				if (file.EndOfStream)
+					endOfFile = true;
+
+				int calories = Convert.ToInt32(caloriesString);
+
+				elfInventory.Add(calories);
+			}
+
+			yield return elfInventory;
+		}
+	}
+}
---
+++ AdventCode2022/Program.cs
@@ -0,0 +1,11 @@
+﻿using AdventCode2022.Day1;
+
+ElfExpedition elfExpedition = new();
+
+int[] results = elfExpedition.Solutions();
+
+Console.WriteLine(results[0]);
+
+Console.WriteLine(results[1]);
+
+Console.ReadKey();
